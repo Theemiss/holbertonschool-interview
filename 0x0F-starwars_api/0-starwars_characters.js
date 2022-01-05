7514067a70ec5a9b54e17f684644543928c9fc60
Ahmed Belhaj <58787184+Theemiss@users.noreply.github.com>
2022-01-05 10:17:27 +0100

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -5,7 +5,7 @@ const id = process.argv[2];
 request('https://swapi-api.hbtn.io/api/films/' + id + '', function (error, response, body) {
   const data = JSON.parse(body);
   const characterslinks = data.characters;
-  characterslinks.forEach(element => {
+  JSON.parse(body).characters.forEach(element => {
     request(element, function (error, response, body) {
       const d = JSON.parse(body);
       console.log(d.name);
