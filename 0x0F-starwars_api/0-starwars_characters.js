97c45dc41e9b5bd9ad04bbd54d0a05dd8204f28c
Theemiss <midinfotn401@gmail.com>
2022-01-05 09:54:01 +0100

    update

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Star Wars Characters
+const request = require('request');
+const id = process.argv[2];
+request('https://swapi-api.hbtn.io/api/films/' + id + '', function (error, response, body) {
+  const data = JSON.parse(body);
+  const characterslinks = data.characters;
+  characterslinks.forEach(element => {
+    request(element, function (error, response, body) {
+      const d = JSON.parse(body);
+      console.log(d.name);
+    });
+  });
+});
