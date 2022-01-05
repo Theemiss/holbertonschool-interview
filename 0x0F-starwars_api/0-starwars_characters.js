541b3979d7d2bffe43babf57b943521297313d12
Theemiss <midinfotn401@gmail.com>
2022-01-05 09:22:22 +0000

    fix

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,7 +4,6 @@ const request = require('request');
 const id = process.argv[2];
 request('https://swapi-api.hbtn.io/api/films/' + id + '', function (error, response, body) {
   const data = JSON.parse(body);
-  const characterslinks = data.characters;
   JSON.parse(body).characters.forEach(element => {
     request(element, function (error, response, body) {
       const d = JSON.parse(body);
