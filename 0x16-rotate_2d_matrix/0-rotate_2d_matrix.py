794996cb4c9b81dadf597c94440f265d9afcfa0b
Theemiss <midinfotn401@gmail.com>
2022-01-18 12:55:17 +0100

    rotate matrix

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+ rotate_2d_matrix module
+
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    rotate 90 degree clockwise
+    """
+    N = len(matrix[0])
+    for i in range(N // 2):
+        for j in range(i, N - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[N - 1 - j][i]
+            matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j]
+            matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i]
+            matrix[j][N - 1 - i] = temp
+    return matrix
