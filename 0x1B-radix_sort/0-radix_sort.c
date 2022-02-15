708e91a0046b7841c54efaedd2c23975c0f4f118
Theemiss <midinfotn401@gmail.com>
2022-02-15 15:34:09 +0100

    new project radix sort

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+ * radix_sort - Sort an array using radix sort
+ *@array: array to sort
+ *@size: size of the array
+ */
+void radix_sort(int *array, size_t size)
+{
+
+}
---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,10 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+void radix_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif /* _SORT_H_ */
