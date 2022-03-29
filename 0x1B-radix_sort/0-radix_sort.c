ca4ec48d14b0955d10dff9553c697bcf0fe98b8e
Theemiss <midinfotn401@gmail.com>
2022-03-29 15:04:23 +0100

    added LSD radix sort

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -19,45 +19,53 @@ int find_max(int *arr, size_t len)
 	return (max);
 }
 /**
+ * radix_count_sort - counts the number of digits in the maximum value
+ * @array: array to be sorted
+ * @size: size of the array
+ * @dig: Integer by which the array is divided to get the digit
+ * Return: Void
+ */
+void radix_count_sort(int *array, int size, int dig)
+{
+	int *bucket, *bucket_cnt, i;
+
+	bucket = malloc(sizeof(int) * size);
+	bucket_cnt = malloc(sizeof(int) * (find_max(array, size) + 1));
+	if (bucket_cnt == NULL || bucket == NULL)
+	{
+		free(bucket_cnt);
+		free(bucket);
+	}
+	for (i = 0; i <= find_max(array, size); i++)
+		bucket_cnt[i] = 0;
+	for (i = 0; i < size; i++)
+		bucket_cnt[array[i] / dig % 10]++;
+	for (i = 1; i <= find_max(array, size); i++)
+		bucket_cnt[i] += bucket_cnt[i - 1];
+	for (i = size - 1; i >= 0; i--)
+		bucket[bucket_cnt[array[i] / dig % 10]-- - 1] = array[i];
+	for (i = 0; i < size; i++)
+		array[i] = bucket[i];
+	free(bucket_cnt);
+	free(bucket);
+}
+/**
  * radix_sort - Sort an array using radix sort
  *@array: array to sort
  *@size: size of the array
  */
 void radix_sort(int *array, size_t size)
 {
-	size_t bucket[10][10], bucket_cnt[10];
-	size_t i, j, k, r, NOP = 0, divisor = 1, lar, pass;
 
-	if (array == NULL && size == 0)
+	int i;
+
+	if (array == NULL || size < 1)
+	{
 		return;
-	lar = find_max(array, size);
-	while (lar > 0)
-	{
-		NOP++;
-		lar /= 10;
 	}
-	for (pass = 0; pass < NOP; pass++)
+	for (i = 1; i < 100; i *= 10)
 	{
-		for (i = 0; i < 10; i++)
-		{
-			bucket_cnt[i] = 0;
-		}
-		for (i = 0; i < size; i++)
-		{
-			r = (array[i] / divisor) % 10;
-			bucket[r][bucket_cnt[r]] = array[i];
-			bucket_cnt[r] += 1;
-		}
-		i = 0;
-		for (k = 0; k < 10; k++)
-		{
-			for (j = 0; j < bucket_cnt[k]; j++)
-			{
-				array[i] = bucket[k][j];
-				i++;
-			}
-		}
-		divisor *= 10;
+		radix_count_sort(array, size, i);
 		print_array(array, size);
 	}
 }
