bedfd0de8c2089e4fc56be7c4de495329b0bca8c
Theemiss <midinfotn401@gmail.com>
2022-03-29 16:31:35 +0100

    update

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -8,17 +8,8 @@ def island_perimeter(grid):
     """
     calculate the perimeter of a grid island
     """
-    row, col = len(grid), len(grid[0])
-    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
     perimeter = 0
-    for r in range(row):
-        for c in range(col):
-            adjacent_land = 0
-            if grid[r][c] == 1:
-                for d in directions:
-                    ro = r + d[0]
-                    co = c + d[1]
-                    if 0 <= ro < row and 0 <= co < col and grid[ro][co] == 1:
-                        adjacent_land += 1
-    perimeter += (4 - adjacent_land)
+    for row in grid + list(map(list, zip(*grid))):
+        for i, j in zip([0] + row, row + [0]):
+            perimeter += int(i != j)
     return perimeter
