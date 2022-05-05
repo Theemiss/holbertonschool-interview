63d33739f40e240bd23de792a207832e5bfddec9
Theemiss <midinfotn401@gmail.com>
2022-05-05 23:14:15 +0100

    regex task

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -7,5 +7,28 @@
  */
 int regex_match(char const *str, char const *pattern)
 {
-
+    if (*pattern == '\0' && *str == '\0')
+        return (1);
+    if (*pattern == '\0' || *str == '\0')
+        return (0);
+    if (*pattern == '.' && *(pattern + 1) == '*' && *(pattern + 2) == '\0')
+        return (1);
+    if (*(pattern + 1) == '*')
+    {
+        if (*(pattern) == '*')
+        {
+            return (regex_match(str, pattern + 2) || regex_match(str + 1, pattern));
+        }
+        if (*(pattern) != *str)
+            return (regex_match(str, pattern + 2));
+        if (*(pattern) == *str && *(str + 1) == *str)
+            return (regex_match(str + 1, pattern));
+        if (*(str + 1) != *str)
+            return (regex_match(str + 1, pattern + 2));
+    }
+    if (*pattern == *str || *pattern == '.')
+    {
+        return (regex_match(str + 1, pattern + 1));
+    }
+    return (0);
 }
