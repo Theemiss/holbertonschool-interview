bc41f3d32454222531a0b061b1245e95e329aa61
Theemiss <midinfotn401@gmail.com>
2022-05-05 22:50:52 +0100

    init regex project

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,11 @@
+#include "regex.h"
+/**
+ * regex_match - checks whether a given pattern matches a given string
+ * @str: is the string to scan
+ * @pattern: is the regular expression
+ * Return: return 1 if the pattern matches the string, or 0 if it doesn’t
+ */
+int regex_match(char const *str, char const *pattern)
+{
+
+}
