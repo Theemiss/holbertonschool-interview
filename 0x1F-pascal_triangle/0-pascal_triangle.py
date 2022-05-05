510ba6e061720237d269b86061c8f3e6481c78a0
Theemiss <midinfotn401@gmail.com>
2022-05-05 22:39:45 +0100

    update

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Pascal Triangle Model
+"""
+
+
+def CofBinom(n, k):
+    """
+    Binomial coefficient
+    C(line, i)   = line! / ( (line-i)! * i! )
+    """
+    res = 1
+    if (k > n - k):
+        k = n - k
+    for i in range(0, k):
+        res = res * (n - i)
+        res = res // (i + 1)
+
+    return res
+
+
+def pascal_triangle(n):
+    """
+     returns a list of lists of integers
+     representing the Pascal triangle of n
+    """
+    triangle = []
+    if n <= 0:
+        return (list)
+    for i in range(n):
+        row = []
+        for j in range(i+1):
+            row.append(CofBinom(i, j))
+        triangle.append(row)
+    return triangle
