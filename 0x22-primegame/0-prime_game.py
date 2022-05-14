775a21dbd2e9475832b9e883e1d9f214979db3db
Theemiss <midinfotn401@gmail.com>
2022-05-14 15:56:44 +0100

    updat

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -3,8 +3,47 @@
 Prime Game
 """
 
+
+def Prime(num):
+    '''
+    if a number is prime Return True Otherwise False
+    '''
+    if num > 1:
+        for i in range(2, int(num/2)+1):
+            if (num % i) == 0:
+                return False
+        return True
+    else:
+        return False
+
+
 def isWinner(x, nums):
     """
-    Prime Game Is
+    Prime Game
     """
-    pass
+    rounds = {
+        "Maria": 0,
+        "Ben": 0
+    }
+    t = 0
+    for i in range(x):
+        choices = [i for i in range(1, nums[i] + 1)]
+        for j in choices:
+            if (Prime(j)):
+                for k in choices:
+                    if k % j == 0:
+                        choices.remove(k)
+                    t = t + 1 % 2
+        if t == 0:
+            rounds["Maria"] += 1
+            t = 1
+        else:
+            rounds["Ben"] += 1
+            t = 0
+
+    if rounds["Maria"] > rounds["Ben"]:
+        return "Ben"
+    elif rounds["Maria"] < rounds["Ben"]:
+        return "Maria"
+    else:
+        return None
